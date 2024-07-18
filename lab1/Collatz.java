e535a983080264cbab4e41e5705b16aca7d22ade
kyee22 <buaakyee22@163.com>
2024-07-18 21:46:43 +0800

    lab4-b

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -3,25 +3,25 @@
  */
 public class Collatz {
     /** Buggy implementation of nextNumber! */
-    public static int nextNumber(int n) {
-        if (n  == 128) {
-            return 1;
-        } else if (n == 5) {
-            return 3 * n + 1;
-        } else {
-            return n * 2;
-        }
-    }
-    ///** Buggy implementation of nextNumber! */
     //public static int nextNumber(int n) {
-    //    if (n  == 1) {
+    //    if (n  == 128) {
     //        return 1;
-    //    } else if (n % 2 == 0) {
-    //        return n / 2;
+    //    } else if (n == 5) {
+    //        return 3 * n + 1;
     //    } else {
-    //        return n * 3 + 1;
+    //        return n * 2;
     //    }
     //}
+    /** Buggy implementation of nextNumber! */
+    public static int nextNumber(int n) {
+        if (n  == 1) {
+            return 1;
+        } else if (n % 2 == 0) {
+            return n / 2;
+        } else {
+            return n * 3 + 1;
+        }
+    }
 
     public static void main(String[] args) {
         int n = 5;
