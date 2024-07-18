c579d25e7453b63cc9811748c81ffa13cbcad432
kyee22 <buaakyee22@163.com>
2024-07-18 21:44:51 +0800

    lab4-a

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -24,7 +24,7 @@ public class Collatz {
     //}
 
     public static void main(String[] args) {
-        int n = 999;
+        int n = 5;
         System.out.print(n + " ");
         while (n != 1) {
             n = nextNumber(n);
